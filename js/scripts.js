4a6da80e6fb3487bc1eb23a479020544d55efb58
Arnar-Sig <enteko5@gmail.com>
2021-05-21 22:17:35 +0000

    laga scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,21 @@
-const fs = require('fs');
-const files = fs.readdirSync('./');
-console.log(files);
+var folder = "img/";
+$.ajax({
+    url : folder,
+    success: function (data) {
+        $(data).find("a").attr("href", function (i, val) {
+            if( val.match(/\.(jpe?g|png|gif|JPG)$/) ) { 
+                let mynd = `
+                <div class="col col-3 col-md-5 col-sm-10 offset-col-md-1 offset-col-sm-1">
+                    <section class="flower">
+                    <a href="${folder + val}" data-lightbox="mygallery" data-title="blóm">
+                        <div class="image"><img src="${folder + val}" alt=""></div
+                        ></a>
+                    </section>
+                </div>
+                `
+                $(galleri).append(mynd);
+                console.log("success");
+            } 
+        });
+    }
+});
