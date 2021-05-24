039a6b4e78977ce2d73c3ecfdfd1a06383a8132f
Arnar-Sig <enteko5@gmail.com>
2021-05-24 19:53:36 +0000

    added skeleton of better adding of elements in script.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,17 @@
 //Loads all images after page load
 let folder = "img/";
-let imgNumber = 0;
+
+$.ajax({
+    url : folder,
+    success: function (data) {
+        $(data).find("a").attr("href", function (i, val) {
+            if( val.match(/\.(jpe?g|png|gif|JPG)$/) ) { 
+   
+            } 
+        });
+    }
+});
+
 $.ajax({
     url : folder,
     success: function (data) {
