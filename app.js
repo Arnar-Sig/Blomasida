eb145c6ea02c23aacd7f4cd13828a14a8a271a32
Arnar-Sig <enteko5@gmail.com>
2021-05-29 00:29:16 +0000

    first version using express and ejs, seems to work?

---
+++ app.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const app = express();
+const path = dirname(fileURLToPath(import.meta.url));
+
+app.use(express.static(join(path, '/public')));
+app.set('views', join(path, '/views'));
+app.set('view engine', 'ejs');
+
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+app.listen(port, hostname, () => {
+  console.info(`Server running at http://${hostname}:${port}/`);
+});
+
+app.use('/', (req, res) => {
+    res.render('../index');
+});
